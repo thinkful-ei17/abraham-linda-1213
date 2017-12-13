c69e46625975c541758f100f7002042279f0290d
Abraham Cuenca <websiteprojectmanager@gmail.com>
2017-12-13 09:17:29 -0800

    Add youtube functions

---
+++ index.js
@@ -0,0 +1,169 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+/*
+// Global variables for GoogleAuth object, auth status.
+var GoogleAuth;
+
+/**
+   * Load the API's client and auth2 modules.
+   * Call the initClient function after the modules load.
+   * /
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+  // Initialize the gapi.client object, which app uses to make API requests.
+  // Get API key and client ID from API Console.
+  // 'scope' field specifies space-delimited list of access scopes
+
+  gapi.client.init({
+    'clientId': 'REPLACE_ME',
+    'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+    'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
+  }).then(function () {
+    GoogleAuth = gapi.auth2.getAuthInstance();
+
+    // Listen for sign-in state changes.
+    GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+    // Handle initial sign-in state. (Determine if user is already signed in.)
+    setSigninStatus();
+
+    // Call handleAuthClick function when user clicks on "Authorize" button.
+    $('#execute-request-button').click(function() {
+      handleAuthClick(event);
+    }); 
+  });
+}
+
+function handleAuthClick(event) {
+  // Sign user in after click on auth button.
+  GoogleAuth.signIn();
+}
+
+function setSigninStatus() {
+  var user = GoogleAuth.currentUser.get();
+  isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
+  // Toggle button text and displayed statement based on current auth status.
+  if (isAuthorized) {
+    defineRequest();
+  }
+}
+
+function updateSigninStatus(isSignedIn) {
+  setSigninStatus();
+}
+
+function createResource(properties) {
+  var resource = {};
+  var normalizedProps = properties;
+  for (var p in properties) {
+    var value = properties[p];
+    if (p && p.substr(-2, 2) == '[]') {
+      var adjustedName = p.replace('[]', '');
+      if (value) {
+        normalizedProps[adjustedName] = value.split(',');
+      }
+      delete normalizedProps[p];
+    }
+  }
+  for (var p in normalizedProps) {
+    // Leave properties that don't have values out of inserted resource.
+    if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
+      var propArray = p.split('.');
+      var ref = resource;
+      for (var pa = 0; pa < propArray.length; pa++) {
+        var key = propArray[pa];
+        if (pa == propArray.length - 1) {
+          ref[key] = normalizedProps[p];
+        } else {
+          ref = ref[key] = ref[key] || {};
+        }
+      }
+    }
+    }
+  return resource;
+}
+
+function removeEmptyParams(params) {
+  for (var p in params) {
+    if (!params[p] || params[p] == 'undefined') {
+      delete params[p];
+    }
+  }
+  return params;
+}
+
+function executeRequest(request) {
+  request.execute(function(response) {
+    console.log(response);
+  });
+}
+
+function buildApiRequest(requestMethod, path, params, properties) {
+  params = removeEmptyParams(params);
+  var request;
+  if (properties) {
+    var resource = createResource(properties);
+    request = gapi.client.request({
+      'body': resource,
+      'method': requestMethod,
+      'path': path,
+      'params': params
+    });
+  } else {
+    request = gapi.client.request({
+      'method': requestMethod,
+      'path': path,
+      'params': params
+    });
+  }
+  executeRequest(request);
+}
+
+*/
+
+
+
+function renderResult(result) {
+  return `
+    <div>
+      <h2>
+      <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
+      <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
+      <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
+    </div>
+  `;
+}
+
+function getVideosFromApi(searchTerm, callback){
+  const query = {
+    'key': '',
+    'maxResults': '4',
+    'safeSearch':'strict',
+    'part': 'snippet',
+    'q': `${searchTerm}`,
+    'type': 'string'
+  };
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+  
+function displayYoutubeSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val('');
+    getVideosFromApi(query, displayYoutubeSearchData);
+  });
+}
+
+$(watchSubmit);
