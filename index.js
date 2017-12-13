3b8a571c779cd6b2e388318ab6e9faf2744f4c43
Abraham Cuenca <websiteprojectmanager@gmail.com>
2017-12-13 11:36:33 -0800

    Add store and update api methods

--- index.js
+++ index.js
@@ -1,153 +1,39 @@
 'use strict';
+/* global $*/
 
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-/*
-// Global variables for GoogleAuth object, auth status.
-var GoogleAuth;
-
-/**
-   * Load the API's client and auth2 modules.
-   * Call the initClient function after the modules load.
-   * /
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
+const store = {
+  videos: []
 }
-
-function initClient() {
-  // Initialize the gapi.client object, which app uses to make API requests.
-  // Get API key and client ID from API Console.
-  // 'scope' field specifies space-delimited list of access scopes
-
-  gapi.client.init({
-    'clientId': 'REPLACE_ME',
-    'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
-    'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
-  }).then(function () {
-    GoogleAuth = gapi.auth2.getAuthInstance();
-
-    // Listen for sign-in state changes.
-    GoogleAuth.isSignedIn.listen(updateSigninStatus);
-
-    // Handle initial sign-in state. (Determine if user is already signed in.)
-    setSigninStatus();
-
-    // Call handleAuthClick function when user clicks on "Authorize" button.
-    $('#execute-request-button').click(function() {
-      handleAuthClick(event);
-    }); 
-  });
-}
-
-function handleAuthClick(event) {
-  // Sign user in after click on auth button.
-  GoogleAuth.signIn();
-}
-
-function setSigninStatus() {
-  var user = GoogleAuth.currentUser.get();
-  isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
-  // Toggle button text and displayed statement based on current auth status.
-  if (isAuthorized) {
-    defineRequest();
-  }
-}
-
-function updateSigninStatus(isSignedIn) {
-  setSigninStatus();
-}
-
-function createResource(properties) {
-  var resource = {};
-  var normalizedProps = properties;
-  for (var p in properties) {
-    var value = properties[p];
-    if (p && p.substr(-2, 2) == '[]') {
-      var adjustedName = p.replace('[]', '');
-      if (value) {
-        normalizedProps[adjustedName] = value.split(',');
-      }
-      delete normalizedProps[p];
-    }
-  }
-  for (var p in normalizedProps) {
-    // Leave properties that don't have values out of inserted resource.
-    if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
-      var propArray = p.split('.');
-      var ref = resource;
-      for (var pa = 0; pa < propArray.length; pa++) {
-        var key = propArray[pa];
-        if (pa == propArray.length - 1) {
-          ref[key] = normalizedProps[p];
-        } else {
-          ref = ref[key] = ref[key] || {};
-        }
-      }
-    }
-    }
-  return resource;
-}
-
-function removeEmptyParams(params) {
-  for (var p in params) {
-    if (!params[p] || params[p] == 'undefined') {
-      delete params[p];
-    }
-  }
-  return params;
-}
-
-function executeRequest(request) {
-  request.execute(function(response) {
-    console.log(response);
-  });
-}
-
-function buildApiRequest(requestMethod, path, params, properties) {
-  params = removeEmptyParams(params);
-  var request;
-  if (properties) {
-    var resource = createResource(properties);
-    request = gapi.client.request({
-      'body': resource,
-      'method': requestMethod,
-      'path': path,
-      'params': params
-    });
-  } else {
-    request = gapi.client.request({
-      'method': requestMethod,
-      'path': path,
-      'params': params
-    });
-  }
-  executeRequest(request);
-}
-
-*/
-
 
 
 function renderResult(result) {
-  let printTitle = result.snippet.title;
-  let displayThumbnailUrl = result.snippet.thumbnails.default.url;
-  let displayThumbnailWidth = result.snippet.thumbnails.default.width;
-  let displayThumbnailHeight = result.snippet.thumbnails.default.height;
-  let embedLink = displayThumbnailHeight = result.id.videoID;
+
   return `
     <div>
-      <p> ${printTitle} </p>
-      <iframe src="http://www.youtube.com/embed/${embedLink}"
-      width="560" height="315" frameborder="0" allowfullscreen></iframe>
-      <img class="js-result-thumbnail" src="${displayThumbnailUrl}" alt="image" height="${displayThumbnailHeight}" width="${displayThumbnailWidth}">
+      <p> ${result.printTitle} </p>
+      <img class="js-result-thumbnail" src="${result.displayThumbnailUrl}" alt="image" height="${result.displayThumbnailHeight}" width="${result.displayThumbnailWidth}">
     </div>
   `;
 }
 
 
+function generateEmbededVideo(){
+ return `<iframe src="http://www.youtube.com/embed/${embedLink}"
+  width="560" height="315" frameborder="0" allowfullscreen></iframe>`
+}
+
+function handleThumbClicked(){
+  $('.js-search-results').on('click', '.js-result-thumbnail', function(){
+  console.log('A thumbnail was clicked');
+  });
+}
+
+
 function getVideosFromApi(searchTerm, callback){
   const query = {
-    'key': 'AIzaSyA3MLZQz9_qUiSL6qM-zKL6igf6KxR6ckA',
+    'key': 'AIzaSyA7d_pPgxIQP-QxDzHnkK3SBq_nOQOV3Wk',
     'maxResults': '4',
     'safeSearch':'strict',
     'part': 'snippet',
@@ -159,9 +45,23 @@ function getVideosFromApi(searchTerm, callback){
 
   
 function displayYoutubeSearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item));
+  if (store.videos.length >= 4) store.videos = [];
+
+  const results = data.items.map((item, index) => {
+    store.videos.push(
+      { printTitle : item.snippet.title,
+        displayThumbnailUrl : item.snippet.thumbnails.default.url,
+        displayThumbnailWidth : item.snippet.thumbnails.default.width,
+        displayThumbnailHeight : item.snippet.thumbnails.default.height,
+        embedLink :  item.id.videoId
+      }
+    );
+  }
+);
+
+    store.videos.map(item => renderResult(item));
+
   $('.js-search-results').html(results);
-  console.log(data);
 }
 
 function watchSubmit() {
@@ -173,6 +73,8 @@ function watchSubmit() {
     queryTarget.val('');
     getVideosFromApi(query, displayYoutubeSearchData);
   });
+
+  handleThumbClicked();
 }
 
 $(watchSubmit);
